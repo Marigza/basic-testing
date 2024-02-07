05a0fc6498d55ce086d23969484c334dedfbc4b9
Marigza <marigola@mail.ru>
2024-02-07 14:20:48 +0300

    feat: simple test

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,67 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const testingFunction = simpleCalculator({
+      a: 1,
+      b: 3,
+      action: Action.Add,
+    });
+    expect(testingFunction).toBe(4);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const testingFunction = simpleCalculator({
+      a: 10,
+      b: 3,
+      action: Action.Subtract,
+    });
+    expect(testingFunction).toBe(7);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const testingFunction = simpleCalculator({
+      a: 10,
+      b: 3,
+      action: Action.Multiply,
+    });
+    expect(testingFunction).toBe(30);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const testingFunction = simpleCalculator({
+      a: 33,
+      b: 3,
+      action: Action.Divide,
+    });
+    expect(testingFunction).toBe(11);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const testingFunction = simpleCalculator({
+      a: 2,
+      b: 3,
+      action: Action.Exponentiate,
+    });
+    expect(testingFunction).toBe(8);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const testingFunction = simpleCalculator({
+      a: 2,
+      b: 3,
+      action: '**',
+    });
+    expect(testingFunction).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const testingFunction = simpleCalculator({
+      a: 'one',
+      b: 'two',
+      action: Action.Add,
+    });
+    expect(testingFunction).toBeNull();
   });
 });
